97ad94080d2ddaa6740613a8323620a3faf57657
Gilberto2025123 <muxindadasilva@gmail.com>
2026-03-24 16:58:28 +0000

    newListItems

--- strike-ToDoList/src/components/TaskBox/TaskBox.jsx
+++ strike-ToDoList/src/components/TaskBox/TaskBox.jsx
@@ -3,12 +3,21 @@ import { useState } from "react"
 
 const TaskBox = () => {
 
-const [ input, setInput] = useState(true)
+const [addtext, setAddText] = useState(true)
 
-const [ addinputbox, setAddInputbox ] = useState(true)
+const newListItems = () => {
+ 
+return (
+    newListItems(addtext)
+)
 
-const handleInputItems = (input) => {
-  return( <input defaultChecked /> &&  setInput(true))
+
+
+}
+
+
+// const handleInputItems = (input) => {
+//   return( <input defaultChecked /> &&  setInput(true))
   
    
  
@@ -16,14 +25,18 @@ const handleInputItems = (input) => {
 }
 
     return (
-     <> 
+    
 <div>
-     <input defaultValue={`Add your task here...`} className="taskinput" {...addinputbox ? input: handleInputItems }/>
-     <button className ="blackcross" handleInput={handleInputItems} img src="" alt=""> + </button>  
+     <input defaultValue={`Add your task here...`} className="taskinput" />
+     <button className ="blackcross" handleInput={handleInputItems} img src="" alt=""> + </button>
+
+     <label input>{newListItems}</label>
+     <label input>{newListItems}</label>
+     <label input>{newListItems}</label>  
      
  </div> 
-     </>
+     
     )
-} 
+
 
 export default TaskBox
